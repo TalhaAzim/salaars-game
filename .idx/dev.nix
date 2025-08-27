733d43cc698f8ac3a7ecf1c79a0f591eded04663
Talha A. B. <talha.azim@gmail.com>
2025-08-27 19:28:54 +0000

    Fixed dev.nix import issue in dev shell

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,19 +1,22 @@
+
 # To learn more about how to use Nix to configure your environment
 # see: https://developers.google.com/idx/guides/customize-idx-env
 { pkgs, ... }: {
   # Which nixpkgs channel to use.
   channel = "stable-23.11"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
-  packages = [
-    pkgs.nodejs_20
-    pkgs.python3
+  packages = with pkgs; [
+    nodejs_20
+    python3
   ];
   # Sets environment variables in the workspace
-  env = {};
+  env = {
+  };
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
-      # "vscodevim.vim"
+      "vscodevim.vim"
+      "Google.validation-agent-extension"
     ];
     # Enable previews and customize configuration
     previews = {
--- flake.nix
+++ flake.nix
@@ -1,22 +1,30 @@
+
 {
   description = "Dev shell for Salaar's communication game";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    idx = {
+      url = "path:./.idx";
+      flake = false;
+    };
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }@inputs:
+  outputs = { self, nixpkgs, idx, flake-utils }@inputs:
     flake-utils.lib.eachDefaultSystem (system:
       let
+	inherit idx;
         pkgs = nixpkgs.legacyPackages.${system};
-	idx = import ./.idx/dev.nix pkgs;
+        dev-dot-nix = import "${idx}/dev.nix" { inherit pkgs; };
       in
       {
         devShells.default = pkgs.mkShell {
           name = "saga";
-	  packages = idx.packages;
-	  shelHook = "";
+	  packages = dev-dot-nix.packages;
+	  shellHook = ''
+	    echo Entering Dev Shell
+	  '';
 	};
       });
 }
